2f68eec24483b9c447ae5483e5a9daa887485eee
Peter Park <parkdhp@gmail.com>
2018-09-06 21:12:04 -0400

    solution

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -1,9 +1,43 @@
-//SELECT one db to work with
-//For SQL
-const sqlDb = require('../../db/sql');
-//For Mongo
-const mongoDb = require('../../db/mongodb')
+const connection = require('../../db/sql');
+
+const getAllFavorites = function(callback) {
+  const myQuery = `SELECT * FROM favorites;`
+  connection.query(myQuery, (err, docs) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, docs);
+    }
+  });
+};
+
+const saveFavorite = function(movie, callback) {
+  let { id, title, vote_average, vote_count, poster_path, backdrop_path, release_date } = movie;
+  let queryString = `INSERT INTO favorites (id, title, vote_average, vote_count, poster_path, backdrop_path, release_date) VALUES (${id}, '${title}', ${vote_average}, ${vote_count}, '${poster_path}', '${backdrop_path}', '${release_date}');`;
+
+  connection.query(queryString, (err, success) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, success)
+    }
+  });
+  
+};
+
+const deleteFavorite = function(movieID, callback) {
+  const queryString = `DELETE FROM favorites WHERE id = '${movieID}'`;
+  connection.query(queryString, (err, success) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, success);
+    }
+  });
+};
 
 module.exports = {
-
-}
+  getAllFavorites,
+  saveFavorite,
+  deleteFavorite,
+};
